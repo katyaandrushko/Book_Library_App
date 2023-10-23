79c56e144bdcdea7116d5a0ed830581d4b9ef203
katyaandrushko <katyaandrushko14@gmail.com>
2023-10-23 13:20:03 +0300

    Add book id

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,9 +1,24 @@
 import './BookList.css'
+import { useSelector } from 'react-redux/es/hooks/useSelector'
 
 const BookList = () => {
+  const books = useSelector((state) => state.books) // підписатися на стан будь-якої частини компонен в додатку
   return (
     <div className="app-block book-list">
       <h2>BookList</h2>
+      {books.lenght === 0 ? (
+        <p>No books available</p>
+      ) : (
+        <ul>
+          {books.map((book, i) => (
+            <li key={book.id}>
+              <div className="book-info">
+                {++i}. {book.title} by <strong>{book.author}</strong>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
